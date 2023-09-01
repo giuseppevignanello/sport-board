ef8e7c35cbf76f4c9b34afc0f5d9cc5f83cd5003
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-01 12:46:20 +0200

    add player to board

--- script.js
+++ script.js
@@ -4,12 +4,48 @@ const opponentPlayersElement = document.getElementById("opponent_players");
 
 const numOfSquare = 308;
 const players = 5;
+let selectedPlayer = "";
 
+//add square to board
 for (let i = 0; i < numOfSquare; i++) {
   boardElement.innerHTML += "<div class='square'></div>";
 }
 
+//add players
 for (let i = 1; i < players + 1; i++) {
   ourPlayersElement.innerHTML += `<div class='player our_player'>${i}</div>`;
   opponentPlayersElement.innerHTML += `<div class='player opponent_player'>${i}</div>`;
 }
+
+// player list
+const ourPlayersNodeList = document.querySelectorAll(".our_player");
+
+const ourPlayersArray = Array.from(ourPlayersNodeList);
+
+const opponentPlayersNodeList = document.querySelectorAll(".opponent_player");
+
+const opponentPlayersArray = Array.from(opponentPlayersNodeList);
+
+ourPlayersArray.forEach((ourPlayer, index) => {
+  ourPlayer.addEventListener("click", function () {
+    selectedPlayer = `<div class='player our_player'>${index + 1}</div>`;
+    console.log(selectedPlayer);
+  });
+});
+
+opponentPlayersArray.forEach((opponentPlayer, index) => {
+  opponentPlayer.addEventListener("click", function () {
+    selectedPlayer = `<div class='player opponent_player'>${index + 1}</div>`;
+    console.log(selectedPlayer);
+  });
+});
+
+//squares
+const squaresNodeList = document.querySelectorAll(".square");
+const squaresArray = Array.from(squaresNodeList);
+
+squaresArray.forEach((square) => {
+  square.addEventListener("click", function () {
+    square.innerHTML = selectedPlayer;
+  });
+});
