a2ae2884c1453f2cdafa75fc0c3ee865f89b178d
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-01 12:15:30 +0200

    add players

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const boardElement = document.getElementById("board");
+const ourPlayersElement = document.getElementById("our_players");
+const opponentPlayersElement = document.getElementById("opponent_players");
 
 const numOfSquare = 308;
+const players = 5;
 
 for (let i = 0; i < numOfSquare; i++) {
   boardElement.innerHTML += "<div class='square'></div>";
+}
+
+for (let i = 1; i < players + 1; i++) {
+  ourPlayersElement.innerHTML += `<div class='player our_player'>${i}</div>`;
+  opponentPlayersElement.innerHTML += `<div class='player opponent_player'>${i}</div>`;
 }
